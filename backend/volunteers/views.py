558422fbea82bf407db3f26d8c9d386576bde7ef
MatthewLi88 <matthewhli809@gmail.com>
2025-12-27 15:03:36 -0600

    updated questionaire

--- backend/volunteers/views.py
+++ backend/volunteers/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import VolunteerProfileForm
 
-# Create your views here.
+def questionnaire(request):
+    if request.method == 'POST':
+        form = VolunteerProfileForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('thank_you')
+    else:
+        form = VolunteerProfileForm()
+    return render(request, 'volunteers/questionnaire.html', {'form': form})
+
+def thank_you(request):
+    return render(request, 'volunteers/thank_you.html')

eab775f24abe0b8a6327b739bfac8deeed112138
MatthewLi88 <matthewhli809@gmail.com>
2025-12-27 23:41:04 -0600

    added temp homepage to urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('volunteers/', include('volunteers.urls')),
+    path('', include('home.urls')),
 ]
